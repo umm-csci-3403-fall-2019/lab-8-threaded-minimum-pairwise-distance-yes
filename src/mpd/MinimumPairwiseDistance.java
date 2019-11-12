59864178022dadae1a23247d1abd6b829471dc48
JoshuaHamann <haman127@morris.umn.edu>
2019-11-11 19:51:33 -0600

    Worked on ThreadedMinimumPairwiseDistance created inner classes to divide the threads, also created the threads and start them respectively to their class

--- src/mpd/MinimumPairwiseDistance.java
+++ src/mpd/MinimumPairwiseDistance.java
@@ -2,6 +2,6 @@ package mpd;
 
 public interface MinimumPairwiseDistance {
 
-    long minimumPairwiseDistance(int[] values);
+    long minimumPairwiseDistance(int[] values) throws InterruptedException;
 
 }
--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,89 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+    private long size;
+
+
 
     @Override
-    public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+    public long minimumPairwiseDistance(int[] values) throws InterruptedException {
+        size = values.length;
+        Thread[] ourThreads = new Thread[4];
+        ourThreads[0] = new Thread(new LowerLeft());
+        ourThreads[1] = new Thread(new BottomRight());
+        ourThreads[2] = new Thread(new TopRight());
+        ourThreads[3] = new Thread(new Center());
+        for (int i = 0; i < ourThreads.length; i++){
+            ourThreads[i].start();
+        }
+        //throw new UnsupportedOperationException();
+        for(int i=0; i<ourThreads.length;i++){
+            ourThreads[i].join();
+        }
+
+        //return global number
+        return 0;
     }
+
+    public class LowerLeft implements Runnable{
+
+        public void run() {
+            for (int i = 0; i < values.length; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+    }
+
+    public class BottomRight implements Runnable{
+
+        public void run() {
+            for (int i = 0; i < values.length; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+    }
+
+    public class TopRight implements Runnable{
+
+        public void run() {
+            for (int i = 0; i < values.length; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Center implements Runnable{
+
+        public void run() {
+            for (int i = 0; i < values.length; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+    }
+
 
 }
